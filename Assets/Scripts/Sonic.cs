c46229ce65cab2dd5553e18d267ca596a9d668b6
Anubhaw Arya <anubhaw.arya@gmail.com>
2015-01-03 00:50:33 -0500

    Turning Sonic around

--- Assets/Scripts/Sonic.cs
+++ Assets/Scripts/Sonic.cs
@@ -2,10 +2,9 @@
 using System.Collections;
 
 public class Sonic : MonoBehaviour {
-	public int directionx;
-	int directiony;
-
+	public int directionX;
 	public int speed;
+	bool start=true;
 	Animator animator;
 
 	void Start () {
@@ -13,21 +12,17 @@ public class Sonic : MonoBehaviour {
 	}
 
 	void Update () {
-		if (directionx > 0) {
-			rigidbody2D.velocity= new Vector2(1f*speed,rigidbody2D.velocity.y);
-		} else if (directionx < 0) {
-			rigidbody2D.velocity= new Vector2(-1f*speed,rigidbody2D.velocity.y);
-		} else {
-			rigidbody2D.velocity= new Vector2(0f,rigidbody2D.velocity.y);
+
+		//RaycastHit2D hitInfo=Physics2D.Raycast(rigidbody2D.position,new Vector2(Vector2.right*directionX,0f);
+		if (rigidbody2D.velocity.x == 0) {
+			if(start){
+				start=false;
+			} else{
+				directionX = -directionX;
+			}
 		}
+		rigidbody2D.velocity = new Vector2 (directionX * speed, rigidbody2D.velocity.y);
 		animator.SetFloat ("SpeedX",Mathf.Abs(rigidbody2D.velocity.x));
-		if (rigidbody2D.velocity.y > 0) {
-			directiony = 1;
-		} else if (rigidbody2D.velocity.y < 0) {
-			directiony = -1;
-		} else {
-			directiony = 0;
-				}
-		animator.SetFloat ("DirectionY", directiony);
+		animator.SetFloat ("VelocityY", rigidbody2D.velocity.y);
 	}
 }
