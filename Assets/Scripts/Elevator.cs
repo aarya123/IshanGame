75c62e462b5700c3b6c2bff8f79754b457c5c6db
Anubhaw Arya <anubhaw.arya@gmail.com>
2015-01-03 04:29:39 -0500

    Elevator

---
+++ Assets/Scripts/Elevator.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Elevator : MonoBehaviour {
+	
+	public int deltaX;
+	public int deltaY;
+	public int speed;
+	[SerializeField]
+	Vector3 startVect;
+	[SerializeField]
+	Vector3 endVect;
+	[SerializeField]
+	Vector3 destination;
+	[SerializeField]
+	bool hasPlayer;
+	Rigidbody2D player;
+	void Start () {
+		startVect = transform.position;
+		endVect = new Vector3 (startVect.x+deltaX, startVect.y+deltaY, startVect.z);
+	}
+	
+	void Update () {
+		float step = speed * Time.deltaTime;
+		if (hasPlayer) {
+			transform.position = Vector3.MoveTowards (transform.position, endVect, step);
+			if(transform.position!=endVect){
+				player.velocity=new Vector2(0f,player.velocity.y);
+			}
+		}else{
+			transform.position = Vector3.MoveTowards (transform.position, startVect, step);
+		}
+	}
+	
+	void OnCollisionEnter2D(Collision2D coll) {
+		if (coll.gameObject.tag == "Player"&&Vector2.Dot(coll.contacts [0].normal,Vector2.right) == 0) {
+			//coll.transform.parent=transform;
+			Debug.Log("Hit!");
+			player=coll.gameObject.rigidbody2D;
+			hasPlayer=true;
+		}
+	}
+	
+	void OnCollisionExit2D(Collision2D coll) {
+		if (coll.gameObject.tag == "Player") {
+			//coll.transform.parent=null;
+			player=null;
+			hasPlayer=false;
+		}
+	}
+}
--- Assets/Scripts/Sonic.cs
+++ Assets/Scripts/Sonic.cs
@@ -6,6 +6,7 @@ public class Sonic : MonoBehaviour {
 	public int speed;
 	public Transform edge;
 	public LayerMask wallLayer;
+	public bool DEBUG;
 	Animator animator;
 
 	void Start () {
@@ -13,10 +14,20 @@ public class Sonic : MonoBehaviour {
 	}
 
 	void Update () {
-		RaycastHit2D hitInfo=Physics2D.Linecast(rigidbody2D.position,edge.position,wallLayer);
-		if (hitInfo.collider != null) {
-			directionX = -directionX;
-			transform.Rotate(new Vector3(0f,180f,0f));
+		if (DEBUG) {
+			if (Input.GetKey (KeyCode.LeftArrow)) {
+				directionX = -1;
+			} else if (Input.GetKey (KeyCode.RightArrow)) {
+				directionX = 1;
+			} else {
+				directionX = 0;
+			}
+		} else {
+			RaycastHit2D hitInfo = Physics2D.Linecast (rigidbody2D.position, edge.position, wallLayer);
+			if (hitInfo.collider != null) {
+				directionX = -directionX;
+				transform.Rotate (new Vector3 (0f, 180f, 0f));
+			}
 		}
 		rigidbody2D.velocity = new Vector2 (directionX * speed, rigidbody2D.velocity.y);
 		animator.SetFloat ("SpeedX",Mathf.Abs(rigidbody2D.velocity.x));
