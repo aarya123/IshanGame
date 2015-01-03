1b8bbe33e15b3f8693fc114166a8de59d5efecfa
Ishan Arya <ishan.arya130@gmail.com>
2015-01-02 22:51:10 -0500

    Sonic moving left and right; brick box collider set
    We should edit the speed a little to make it slower

---
+++ Assets/Scripts/Sonic.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sonic : MonoBehaviour {
+	public int direction;
+	public int speed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (direction > 0) {
+			rigidbody2D.velocity= new Vector2(1f*speed,rigidbody2D.velocity.y);
+		} else if (direction < 0) {
+			rigidbody2D.velocity= new Vector2(-1f*speed,rigidbody2D.velocity.y);
+		} else {
+			rigidbody2D.velocity= new Vector2(0f,rigidbody2D.velocity.y);
+		}
+	}
+}
