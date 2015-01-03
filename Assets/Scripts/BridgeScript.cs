71b56689af1b519b5fc8b83203f244e469f2db4d
Ishan Arya <ishan.arya130@gmail.com>
2015-01-03 17:27:23 -0500

    Mouse Positioning and Hitting Objects
    Have to set it to only detect bridge.

--- Assets/Scripts/BridgeScript.cs
+++ Assets/Scripts/BridgeScript.cs
@@ -11,12 +11,12 @@ public class BridgeScript : MonoBehaviour {
 	void Update () {
 
 		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-		RaycastHit2D hit;
-		if (Physics2D.Raycast (ray, hit, 1000)) {
+		RaycastHit2D hit = Physics2D.Raycast (ray.origin, ray.direction, Mathf.Infinity);
+		if (hit) {
 			Debug.Log("You hit: " + hit.collider.gameObject);
 				}
 		Vector2 relative = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-		Debug.Log (relative);
+
 		if (Input.GetMouseButtonDown(0)) {
 			transform.Rotate(new Vector3(0f, 0f, Mathf.Atan (relative.y/relative.x)));
 		}
