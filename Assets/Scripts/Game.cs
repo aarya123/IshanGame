07a27aa85d1351fffa7dcd51c194d738df818055
Anubhaw Arya <anubhaw.arya@gmail.com>
2015-01-03 00:35:45 -0500

    Wall code

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Game : MonoBehaviour {
+	
+	public Camera mainCamera;
+	public BoxCollider2D leftWall;
+	public BoxCollider2D rightWall;
+	public BoxCollider2D topWall;
+	public BoxCollider2D bottomWall;
+
+	void Start () {
+		topWall.size = new Vector2 (mainCamera.ScreenToWorldPoint (new Vector3 (Screen.width * 2f, 0f, 0f)).x, 1f);
+		topWall.center = new Vector2 (0f, mainCamera.ScreenToWorldPoint (new Vector3 (0f, Screen.height, 0f)).y + .5f);
+		
+		bottomWall.size = new Vector2 (mainCamera.ScreenToWorldPoint (new Vector3 (Screen.width * 2f, 0f, 0f)).x, 1f);
+		bottomWall.center = new Vector2 (0f, mainCamera.ScreenToWorldPoint (new Vector3 (0f, 0f, 0f)).y - .5f);
+		
+		leftWall.size = new Vector2 (1f, mainCamera.ScreenToWorldPoint (new Vector3 (0f, Screen.height * 2f, 0f)).y);
+		leftWall.center = new Vector2 (mainCamera.ScreenToWorldPoint (new Vector3 (0f, 0f, 0f)).x - .5f, 0f);
+		
+		rightWall.size = new Vector2 (1f, mainCamera.ScreenToWorldPoint (new Vector3 (0f, Screen.height * 2f, 0f)).y);
+		rightWall.center = new Vector2 (mainCamera.ScreenToWorldPoint (new Vector3 (Screen.width, 0f, 0f)).x + .5f, 0f);
+	}
+}
