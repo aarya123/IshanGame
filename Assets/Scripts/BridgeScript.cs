8a9ab1746c2d21bd6cfb53d9a53cbc5f43ee0c75
Ishan Arya <ishan.arya130@gmail.com>
2015-01-03 17:09:21 -0500

    Very Buggy

--- Assets/Scripts/BridgeScript.cs
+++ Assets/Scripts/BridgeScript.cs
@@ -3,14 +3,22 @@ using System.Collections;
 
 public class BridgeScript : MonoBehaviour {
 	
+
 	void Start () {
 	
 	}
 
 	void Update () {
+
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit2D hit;
+		if (Physics2D.Raycast (ray, hit, 1000)) {
+			Debug.Log("You hit: " + hit.collider.gameObject);
+				}
+		Vector2 relative = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+		Debug.Log (relative);
 		if (Input.GetMouseButtonDown(0)) {
-			Debug.Log(Input.mousePosition);
-			transform.Rotate(new Vector3(0f, 0f, Mathf.Atan (Input.mousePosition.y/Input.mousePosition.x)));
+			transform.Rotate(new Vector3(0f, 0f, Mathf.Atan (relative.y/relative.x)));
 		}
 	}
 }
