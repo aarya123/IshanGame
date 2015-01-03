bbff67f894e8f3589c9563d692cf21c6f7b6b1da
Ishan Arya <ishan.arya130@gmail.com>
2015-01-03 05:00:27 -0500

    Started Bridge Drag & Drop

---
+++ Assets/Scripts/BridgeScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BridgeScript : MonoBehaviour {
+	
+	void Start () {
+	
+	}
+
+	void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+			Debug.Log(Input.mousePosition);
+			transform.Rotate(new Vector3(0f, 0f, Mathf.Atan (Input.mousePosition.y/Input.mousePosition.x)));
+		}
+	}
+}
