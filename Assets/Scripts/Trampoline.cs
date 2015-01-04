dde2b8109040a7348597257234229bc4630a6ad0
Anubhaw Arya <anubhaw.arya@gmail.com>
2015-01-03 19:42:27 -0500

    Remove debug log

--- Assets/Scripts/Trampoline.cs
+++ Assets/Scripts/Trampoline.cs
@@ -8,7 +8,6 @@ public class Trampoline : MonoBehaviour {
 
 	void OnTriggerStay2D(Collider2D other) {
 		if (other.gameObject.tag == "Player") {
-			Debug.Log ("Hit!");
 			other.rigidbody2D.AddForce(new Vector2(velocityX,velocityY));
 			//other.rigidbody2D.velocity+=new Vector2(velocityX,velocityY);
 		}
