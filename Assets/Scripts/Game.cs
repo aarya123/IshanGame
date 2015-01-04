682874e184d4c5732db67776e2c0f57ef0c61773
Anubhaw Arya <anubhaw.arya@gmail.com>
2015-01-03 22:38:26 -0500

    Simplified Game Camera

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -2,14 +2,15 @@
 using System.Collections;
 
 public class Game : MonoBehaviour {
-	
-	public Camera mainCamera;
+
 	public BoxCollider2D leftWall;
 	public BoxCollider2D rightWall;
 	public BoxCollider2D topWall;
 	public BoxCollider2D bottomWall;
 
 	void Start () {
+		Camera mainCamera = Camera.main;
+
 		topWall.size = new Vector2 (mainCamera.ScreenToWorldPoint (new Vector3 (Screen.width * 2f, 0f, 0f)).x, 1f);
 		topWall.center = new Vector2 (0f, mainCamera.ScreenToWorldPoint (new Vector3 (0f, Screen.height, 0f)).y + .5f);
 		
