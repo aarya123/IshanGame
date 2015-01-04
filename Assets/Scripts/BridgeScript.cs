424c1dabfdbc630f3e0a921d7ff6476697c0c3d2
Ishan Arya <ishan.arya130@gmail.com>
2015-01-03 20:12:08 -0500

    Half Coding of the Bridge

--- Assets/Scripts/BridgeScript.cs
+++ Assets/Scripts/BridgeScript.cs
@@ -14,11 +14,12 @@ public class BridgeScript : MonoBehaviour {
 		RaycastHit2D hit = Physics2D.Raycast (ray.origin, ray.direction, Mathf.Infinity);
 		if (hit) {
 			Debug.Log("You hit: " + hit.collider.gameObject);
+			Vector2 relative = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+			Debug.Log (relative);
+			if (Input.GetMouseButton(0)) {
+				transform.Rotate(new Vector3(0f, 0f, Mathf.Atan (relative.y/relative.x)));
+			}
 				}
-		Vector2 relative = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
 
-		if (Input.GetMouseButtonDown(0)) {
-			transform.Rotate(new Vector3(0f, 0f, Mathf.Atan (relative.y/relative.x)));
-		}
 	}
 }
