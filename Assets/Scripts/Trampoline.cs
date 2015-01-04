7907b2bde9cc7056d0fd1e47128f9b13dfc18f5b
Ishan Arya <ishan.arya130@gmail.com>
2015-01-04 00:09:28 -0500

    Revert "Tried to Figure Out Button: Failed"
    This reverts commit 22e4c8b1b383f6ef364cbf8209c1026530bc9414.

--- Assets/Scripts/Trampoline.cs
+++ Assets/Scripts/Trampoline.cs
@@ -5,15 +5,11 @@ public class Trampoline : MonoBehaviour {
 
 	public float velocityX;
 	public float velocityY;
-	public Input button; 
 
 	void OnTriggerStay2D(Collider2D other) {
 		if (other.gameObject.tag == "Player") {
-			if(button){
-				other.rigidbody2D.AddForce(new Vector2(velocityX,velocityY));
-				//other.rigidbody2D.velocity+=new Vector2(velocityX,velocityY);
-			}
-
+			other.rigidbody2D.AddForce(new Vector2(velocityX,velocityY));
+			//other.rigidbody2D.velocity+=new Vector2(velocityX,velocityY);
 		}
 	}
 }
