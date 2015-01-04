720f53c84a70b870b8f0c4bec99acab1bd3e77af
Anubhaw Arya <anubhaw.arya@gmail.com>
2015-01-03 21:37:33 -0500

    Portals

---
+++ Assets/Scripts/Portals.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Portals : MonoBehaviour {
+
+	public Transform otherPortal;
+	bool deferTeleport;
+	void OnTriggerStay2D(Collider2D other) {
+		if (other.gameObject.tag == "Player"&&!deferTeleport) {
+			other.rigidbody2D.position=otherPortal.position;
+			deferTeleport=false;
+			otherPortal.gameObject.SendMessage("defer");
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		if (other.gameObject.tag == "Player") {
+			deferTeleport=false;
+		}
+	}
+
+	void defer(){
+		deferTeleport = true;
+	}
+}
