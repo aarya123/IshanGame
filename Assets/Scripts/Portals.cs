9595cfae8661623059413d6cc9d915ed89bd11bd
Anubhaw Arya <anubhaw.arya@gmail.com>
2015-01-04 00:04:51 -0500

    Button for portals

--- Assets/Scripts/Portals.cs
+++ Assets/Scripts/Portals.cs
@@ -1,25 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Portals : MonoBehaviour {
+public class Portals : MonoBehaviour
+{
 
-	public Transform otherPortal;
-	bool deferTeleport;
-	void OnTriggerStay2D(Collider2D other) {
-		if (other.gameObject.tag == "Player"&&!deferTeleport) {
-			other.rigidbody2D.position=otherPortal.position;
-			deferTeleport=false;
-			otherPortal.gameObject.SendMessage("defer");
-		}
-	}
+    public Transform otherPortal;
+    bool deferTeleport;
+    bool portalsActive;
 
-	void OnTriggerExit2D(Collider2D other) {
-		if (other.gameObject.tag == "Player") {
-			deferTeleport=false;
-		}
-	}
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (portalsActive && other.gameObject.tag == "Player" && !deferTeleport)
+        {
+            other.rigidbody2D.position = otherPortal.position;
+            deferTeleport = false;
+            otherPortal.gameObject.SendMessage("defer");
+        }
+    }
 
-	void defer(){
-		deferTeleport = true;
-	}
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (portalsActive && other.gameObject.tag == "Player")
+        {
+            deferTeleport = false;
+        }
+    }
+
+    void defer()
+    {
+        deferTeleport = true;
+    }
+
+    public void triggerPortals()
+    {
+        portalsActive = !portalsActive;
+    }
 }
